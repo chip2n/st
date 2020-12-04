b9367e3541b67481f2db755a7d053fc8ba6461b0
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-12-04 10:22:08 +0100

    Add custom keybinding for pasting

--- config.def.h
+++ config.def.h
@@ -208,6 +208,9 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+
+        /* custom keybindings */
+	{ ControlMask,          XK_y,           clippaste,      {.i =  0} },
 };
 
 /*
